f620f597cc115d760a122459ce139cd22e8fb602
Aidan K. Ewart <aidanprattewart@gmail.com>
2021-10-20 12:32:49 +0100

    cool ideas are occuring

--- app/Command.hs
+++ app/Command.hs
@@ -3,25 +3,35 @@ import Kernel
 
 import Control.Monad.State
 import Control.Monad.Except
+import Control.Monad.Reader
 import qualified Data.Map as M
 
+{- will just use lua for cool purposes -}
 {- simple imperative metalanguage for manipulating proofs -}
 {- further plans: stepwise execution -}
 
 data ImpError
     = UnknownVar Name
+    | NoFrame Name
 
 type ImpVarState = [M.Map Name CommandVal]
-type Imp = ExceptT ImpError (State ImpVarState)
+type ImpFunctions = [M.Map Name CommandFunc]
+type Imp = ExceptT ImpError (ReaderT ImpFunctions (State ImpVarState))
 
 lookupVar :: Name -> Imp CommandVal
 lookupVar n = do
-    s <- get
-    let lookupAll (m:ms) = case M.lookup n m of
-            Just x -> pure x
-            Nothing -> lookupAll ms
-        lookupAll [] = throwError (UnknownVar n)
-    lookupAll s
+    m <- get
+    case m of
+        (m:_) -> case M.lookup n m of
+            Just v -> pure v
+            Nothing -> throwError (UnknownVar n)
+        [] -> throwError (NoFrame n)
+
+lookupFunc :: Name -> Imp CommandFunc
+lookupFunc n = do
+    m <- ask
+    case M.lookup n m of
+        Just f ->
 
 data Grammar
 
@@ -29,12 +39,13 @@ data CommandVal
     = Proof Proof
     | Prop Term
     | Int Int
+    | Label Name
 
 data CommandExpr
     = Lit CommandVal
     | Var Name
     | Op ([CommandExpr] -> Imp CommandVal) [CommandExpr]
-    | Call Name [CommandExpr]
+    | Call CommandExpr [CommandExpr]
 
 data CommandStmt
     = While CommandExpr [CommandStmt]
@@ -42,6 +53,9 @@ data CommandStmt
     | Expr CommandExpr
     | Assign Name CommandExpr
     | Refine CommandExpr
+    | Return CommandExpr
+
+data CommandFunc = Func [Name] [CommandStmt]
 
 data Command
     = Notation Grammar
@@ -51,7 +65,13 @@ data Command
     | EndProof
 
 evalExpr :: CommandExpr -> Imp CommandVal
+evalExpr (Lit e) = pure e
+evalExpr (Var n) = lookupVar n
+evalExpr (Op fn xs) = mapM evalExpr xs >>= fn
+evalExpr (Call n xs) = mapM 
 
 evalStmt :: CommandStmt -> Imp CommandStmt
+
+evalFunc :: CommandFunc -> [CommandVal] -> Imp CommandVal
 
 evalCommand :: Command -> Imp ()
