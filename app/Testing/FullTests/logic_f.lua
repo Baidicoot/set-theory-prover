7bbec4956c90bcb9da57bd1203695b2670a6762f
Baidicoot <aidanprattewart@gmail.com>
2022-03-31 18:40:38 +0100

    hhh

--- app/Testing/FullTests/logic_f.lua
+++ app/Testing/FullTests/logic_f.lua
@@ -2,7 +2,8 @@ const("some_const","[prop]")
 const("some_other_const","[prop]")
 const("some_other_other_const","[prop]")
 assert("some_axiom","some_const")
-assert("some_other_axiom","[imp some_const some_other_const]")
+assert("some_other_axiom",
+    "[imp some_const some_other_const]")
 
 beginProof("some_other_other_const")
     refine("[modPon some_other_axiom [hole]]")
